ff4d63e6304f62b8ee137c9dcb60bfaf44769f95
莫肇聪 <mozhaocong@qq.com>
2022-08-22 18:03:17 +0800

    feature: 更新

--- app/service/group.ts
+++ app/service/group.ts
@@ -1,23 +1,5 @@
 import BaseService from '../core/Service'
 
 export default class UserService extends BaseService {
-	async findAll() {
-		const user = await this.app.model.Group.findAll()
-		return { user }
-	}
-	async findParams(findSearchParamsRule) {
-		const { simpleParamsRuleModelFindAll } = this
-		return await simpleParamsRuleModelFindAll({
-			that: this,
-			model: this.app.model.Group,
-			paramsRule: findSearchParamsRule
-		})
-	}
-	async create(pramsRules) {
-		const { ctx, getParamsRuleData } = this
-		const { body } = ctx.request
-		const data = getParamsRuleData(body, pramsRules)
-		const user = await this.app.model.Group.create(data)
-		return user
-	}
+	appModel = this.app.model.Group
 }
